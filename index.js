9cc68858a1b5c1c3807dc0ed21c1495bc2639fda
eliashezron <eliashezron23@gmail.com>
2022-06-14 11:44:14 +0300

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-// Import stylesheets
-import './style.css';
+/* Moralis init code */
+const serverUrl = 'https://5zenrjqoogkm.usemoralis.com:2053/server';
+const appId = 'fpV6gOf2DBVPelzeXHevT0gO8S0bDMa6zDmWupPm';
+Moralis.start({ serverUrl, appId });
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+/* Authentication code */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate({
+      signingMessage: 'Log in using Moralis',
+    })
+      .then(function (user) {
+        console.log('logged in user:', user);
+        console.log(user.get('ethAddress'));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log('logged out');
+}
+
+document.getElementById('btn-login').onclick = login;
+document.getElementById('btn-logout').onclick = logOut;
+
